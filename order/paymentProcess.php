182f6c0d670a5b6df04b309fac447bd8b8e0b872
unknown <cgrijalva@cgrijalval-PC.datapro.datapromiami.com>
2015-03-04 19:02:20 -0500

    implemented strip API changes

--- order/paymentProcess.php
+++ order/paymentProcess.php
@@ -1,12 +1,35 @@
 <?php
 	require_once('../stripe-php-2.1.1/init.php');
 
-	if(isset($_POST['order_id']) && isset($_POST['USER_ID'])){
+	if(isset($_POST['order_id']) && isset($_SESSION['USER_ID'])){
+		$order_id 	 = $_POST['order_id'];
+		$user_id  	 = $_SESSION['USER_ID'];
+		$card_number = $_POST['card_number'];
+		$exp_month   = $_POST['exp_month'];
+		$exp_year    = $_POST['exp_year'];
+		$amount 	 = $_POST['amount'];
+	}
 
-	
+	//Split amount taken from form and convert it to cents
+	$split = explode(".", $amount);
+	$dollarToCents = $split[0] * 100;
+	$amount = $dollarToCents + $split[1];
+
+	$card = array('number' => $card_number, 'exp_month' => $exp_month, 'exp_year' => $exp_year);
+
 	try {
 	  // TODO
 	  \Stripe::setApiKey('sk_test_o7fozNe2H2phOAKt2AyPleoW');
+
+	  \Stripe\Charge::create(array(
+	  	'card'				=> $card,
+	  	"amount" 			=> $amount,
+	  	"currency"			=> "usd",
+	  	"source"			=> "tok_15cgmEJYnaXnkuyOcXphZGn0",
+	  	"receipt_email"		=> $email,
+	  	"metadata"	=> array("order_id" => $order_id, "user_id" => $user_id, "email" => $email)
+	  ));
+
 	} catch(\Stripe\Error\Card $e) {
 	  // Since it's a decline, \Stripe\Error\Card will be caught
 	  $body = $e->getJsonBody();
